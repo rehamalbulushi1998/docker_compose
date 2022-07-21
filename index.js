2196508b7ac594e92f0ebf9dee12bc33ac666656
rehamalbulushi1998 <97521035+rehamalbulushi1998@users.noreply.github.com>
2022-07-21 14:25:43 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mysql = require('mysql2');
+
+const mysqlConfig = {
+  host: "mysql_server",
+  user: "reham",
+  password: "reham12345",
+  database: "docker_db"
+}
+
+
+let con = null
+
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+
+app.get('/dbconnection', function (req, res) {
+  con =  mysql.createConnection(mysqlConfig);
+  con.connect(function(err) {
+    if (err) throw err;
+    res.send('connected')
+  });
+})
+
+
+app.get('/getadmin', function (req, res) {
+  con.connect(function(err) {
+    if (err) throw err;
+    //retrive admin name from the database
+    const sql = `SELECT * FROM admin`
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+      res.send(JSON.stringify(result))
+    });
+  });
+})
+
+app.listen(8000)
+
+console.log("listening on port 8000")
+
